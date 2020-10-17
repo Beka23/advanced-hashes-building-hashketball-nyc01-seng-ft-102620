1f419a9e998e8a8439aaafcb07c69c8ecb380ee4
Beksultan <beksultankaiypov@gmail.com>
2020-10-17 19:10:28 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,8 +1,8 @@
 # Write your code here!
 
 def game_hash
-  { :home => { :team_name => "Brooklyn Nets" 
-               :colors => ["Black, White"]
+  { :home => { :team_name => "Brooklyn Nets", 
+               :colors => ["Black, White"],
                :players => [ 
                  {:player_name => "Alan Anderson", :number => 0, :shoe => 16,
                  :points => 22, :rebounds => 12, :assists => 12, 
@@ -27,8 +27,8 @@ def game_hash
     
   }
                
-    :away => { :team_name => "Charlotte Hornets"  
-               :colors => ["Turquoise, Purple"]
+    :away => { :team_name => "Charlotte Hornets",  
+               :colors => ["Turquoise, Purple"],
                :players => [
                    {:player_name => "Jeff Adrien", :number => 4, :shoe => 18,
                  :points => 10, :rebounds => 1, :assists => 1, 
