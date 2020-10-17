fbb1d5feaf3a683165db5215388a6456bf29be27
Beksultan <beksultankaiypov@gmail.com>
2020-10-17 19:05:48 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -30,9 +30,9 @@ def game_hash
     :away => { :team_name => "Charlotte Hornets"  
                :colors => ["Turquoise, Purple"]
                :players => [
-                   {:player_name => "Jeff Adrien", :number => 0, :shoe => 16,
-                 :points => 22, :rebounds => 12, :assists => 12, 
-                 :steals => 3, :blocks => 1, :slam_dunks => 1}, 
+                   {:player_name => "Jeff Adrien", :number => 4, :shoe => 18,
+                 :points => 10, :rebounds => 1, :assists => 1, 
+                 :steals => 2, :blocks => 7, :slam_dunks => 2}, 
                  
                  {:player_name => "Reggie Evans", :number => 30, :shoe => 14,
                  :points => 12, :rebounds => 12, :assists => 12, 
