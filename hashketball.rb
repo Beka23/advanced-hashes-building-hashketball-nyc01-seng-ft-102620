e38b0ba70c71fb49765983db8c51bb5b15e7809c
Beksultan <beksultankaiypov@gmail.com>
2020-10-17 19:03:53 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -20,9 +20,9 @@ def game_hash
                  :points => 26, :rebounds => 11, :assists => 6, 
                  :steals => 3, :blocks => 8, :slam_dunks => 5}, 
                  
-                 {:player_name => "Jason Terry"}, {:number => 31}, {:shoe => 15},
-                 {:points => 19}, {:rebounds => 2}, {:assists => 2}, 
-                 {:steals => 4}, {:blocks => 11}, {:slam_dunks => 1}
+                 {:player_name => "Jason Terry", :number => 31, :shoe => 15,
+                 :points => 19, :rebounds => 2, :assists => 2, 
+                 :steals => 4, :blocks => 11, :slam_dunks => 1}
                   ]
     
   }
